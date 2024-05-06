46d2c6dc1852f4ac7e8a22c1ef868ebe5e0f597d
Darwin  Karl B. Pua <71256936+BananaQueue@users.noreply.github.com>
2024-05-06 14:35:06 +0800

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -2,10 +2,10 @@
 include_once 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password
+    $username = $_POST['userName'];
+    $password = password_hash($_POST['passWord'], PASSWORD_DEFAULT); // Hash the password
 
-    $sql = "INSERT INTO Users (username, password) VALUES ('$username', '$password')";
+    $sql = "INSERT INTO Users (userName, passWord) VALUES ('$username', '$password')";
 
     if (mysqli_query($conn, $sql)) {
         echo "Registration successful";
